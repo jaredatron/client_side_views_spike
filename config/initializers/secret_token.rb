523ad6a6508d709a045a35fa41a430f2b3e9683e
Jared Grippe <jared@deadlyicon.com>
2012-04-16 11:06:13 -0700

    WIP

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ClientSideViewsExample::Application.config.secret_token = 'd26a76ed3be4efa833951eaea951dec9015749d3f182c288ae9fe2874ac8f1dcdb39847b322a36f5ead2f1f8ddd90120373e5928b157219c7b04a76dd803490a'
+ClientSideViewsExample::Application.config.secret_token = '5592daa09454f39a3d669c7ce18453c1f14079cfc8946ecf780164b9ed268f7ce821559dd594dad9ec5c1f8068c464bb1667067eb6206a222788ac7b78678b5d'
